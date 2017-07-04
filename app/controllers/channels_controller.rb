3665883c0ed805231b28bd1ea32bcc7b738a6b7d
sagae keiga <sagae5.28rujeae@gmail.com>
2017-07-04 03:44:43 +0000

    g

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -43,7 +43,7 @@ require 'open-uri'
     
     def scraping2
         @urls2 = Story.all
-        @urls2[14000..20000].each do |url2|
+        @urls2[19000..26000].each do |url2|
             begin
 
             doc = Nokogiri.HTML(open("#{url2.url}"))
